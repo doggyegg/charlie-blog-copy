c697ba741afed517c73c771cb591f0e766bc393d
clh <chenlihuai@chexiao.co>
2025-09-04 18:13:58 +0800

    feat:修改sourceArr

--- interview/sortArr.js
+++ interview/sortArr.js
@@ -1,10 +1,7 @@
 // 不调用arr.sort对数组排序
 
-const sourceArr = [3,6,2,21,32,12,18,32,2];
+const sourceArr = [3, 6, 2, 21, 32, 12, 18, 32, 2, 3];
 
-function sortArr(){
-
-}
+function sortArr() {}
 
 console.log(sortArr(sourceArr));
- 
